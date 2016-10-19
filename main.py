0b2cd48dca7b10013f0629f6581f7c1feacdf5fb
Cristiano Werner Araujo <cristianowerneraraujo@gmail.com>
2016-10-19 19:41:31 -0200

    Files can be passed.

--- main.py
+++ main.py
@@ -74,6 +74,7 @@ if __name__ == '__main__':
     post = subparsers.add_parser("post")
     post.add_argument("-a", "-address", help="Server to post content to", dest="post_server", default="localhost:9090")
     post.add_argument("-c", "-content", help="Content to be posted", dest="post_content")
+    post.add_argument("-f", "-filename", help="File to be posted", dest="post_file", default=None)
     post.add_argument("-t", "-title", help="Title of content to be posted", dest="post_title", default="")
 
     args = p.parse_args()
@@ -93,7 +94,12 @@ if __name__ == '__main__':
     elif args.command ==  "post":
         try:
             import json
-            ans = post_snippet(args.post_server, args.post_content, args.post_title)
+            if args.post_file is not None:
+                content = open(args.post_file).read()
+            else:
+                content = args.post_content
+            ans = post_snippet(args.post_server, content, args.post_title)
+
             if ans.status_code >= 400:
                 print "Error posting, code: {0}".format(ans.status_code)
             link = json.loads(ans.content)["link"]
